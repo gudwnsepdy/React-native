733738f6d78022571215b8090b04e5633e3c4a42
Hyoungjoo Lee <gudwnsepdy@naver.com>
2020-12-14 00:08:18 +0900

    13

--- react_native_01/src/picker.js
+++ react_native_01/src/picker.js
@@ -8,13 +8,32 @@
 
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-
+import {Picker} from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 
 class PickerComponent extends Component {
+
+    state = {
+        contry: 'canada'
+    }
   render() {
       return (
-          <View>
-              <Text>Hello World</Text>
+          <View style={styles.container}>
+              <Slider
+              style={{height: 40, width: 300}}
+              />
+              
+              <Picker style={{height: 50, width: 250}}
+              selectedValue={this.state.contry}
+              onValueChange={(val, idx) =>
+                this.setState({contry: val})
+            }
+
+              >
+                  <Picker.Item label="Korea" value="korea"/>
+                  <Picker.Item label="Canada" value="canada"/>
+              </Picker>
+
           </View>
       )
   }
@@ -22,6 +41,12 @@ class PickerComponent extends Component {
 
 const styles = StyleSheet.create({
  
-  
-})
+    container: {
+        flex: 1,
+        paddingTop: 20,
+        marginBottom: 200,
+        alignItems: 'center'
+    }
+}
+)
 export default PickerComponent;
